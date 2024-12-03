1816e3f2a927baad3275180b02465975d2f4cee0
Matthew Adebayo <olumatty@gmail.com>
2024-12-03 21:07:39 +0100

    second commit

--- src/components/Parapraser.jsx
+++ src/components/Parapraser.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 
 const Parapraser = () => {
+    const [inputText, setInputText] = useState("")
   return (
     <div className='p-10 flex flex-col justify-center items-center '> 
-        <h1 className='text-center text-2xl font-bold '> Text Paraphraser</h1>
+        <h1 className='text-center text-2xl font-bold '>Text Paraphraser</h1>
       <div>
-        <textarea rows={10} className='border mt-3 h-[280px] w-[300px] outline-none rounded'/>
+        <textarea value={inputText} onChange={(e) => setInputText(e.target.value)} rows={10} className='border mt-3 h-[280px] w-[300px] outline-none rounded'/>
         <textarea rows={10} className='border mt-3 h-[280px] w-[300px] outline-none rounded'/>
       </div>
-      <button className='mt-3'>Paraphrase</button>
+      <button className='mt-3 bg-blue-500 w-full h-10 text-white font-bold rounded '>Submit</button>
     </div>
   )
 }
